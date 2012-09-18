1459093cd04cdfb45d40919d2569da244be404b7
manavmanocha <manav.manocha@comprotechnologies.com>
2012-09-18 15:29:29 +0530

    Execute deploy-css when application is run in devlopment mode.

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -5,10 +5,10 @@ eventCompileEnd = {kind->
 	def classLoader = Thread.currentThread().contextClassLoader
 	classLoader.addURL(new File(classesDirPath).toURI().toURL())
 	def config = new ConfigSlurper(GrailsUtil.environment).parse(classLoader.loadClass('Config'))
-	if (!(config.grails?.resources?.debug == null || config.grails.resources.debug == true)) {
-		includeTargets << new File("${cgrailsPluginDir}/scripts/DeployCss.groovy")
-		deployCSS()
-	}
+	//if (!(config.grails?.resources?.debug == null || config.grails.resources.debug == true)) {
+	includeTargets << new File("${cgrailsPluginDir}/scripts/DeployCss.groovy")
+	deployCSS()
+	//}
 }
 
 
